2b2118907a12bd40dabd0283deb897c1debbf932
typeluis <typeluis@gmail.com>
2022-10-23 09:08:36 -0400

    adding pages

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import classes from '../styles/Header.module.scss'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Header = () => {
+    const router = useRouter()
+
+    const navigator = (e) => {
+        // console.log(e)
+        router.push(`/${e.target.textContent}`)
+    }
     return (
         <nav className={classes.navbar}>
             <ul>
-                <li>Home</li>
+                <li onClick={(e) => { navigator(e) }}>Home</li>
                 <li>Shop</li>
                 <li>Donate</li>
                 <li>Videos</li>
