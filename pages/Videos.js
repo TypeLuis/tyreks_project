fb7ff6f737687877c2c216ef18bcdc28e8fd700e
typeluis <typeluis@gmail.com>
2022-10-26 16:45:12 -0400

    adding static props

--- pages/Videos.js
+++ pages/Videos.js
@@ -15,7 +15,7 @@ const Videos = () => {
     const test = async (id) => {
         const response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&fields=items(id%2Csnippet)&key=${process.env.YTAPI}`)
 
-        return response.data.items[0]
+        return response
     }
     return (
         <div className={classes.main}>
@@ -27,11 +27,11 @@ const Videos = () => {
                     const id = item.split('=')[1]
                     // console.log(id)
                     const response = test(id)
-
+                    // console.log(response)
 
                     return (
                         <div className={classes['Iframe_Container']}>
-                            <iframe src={url} allowfullscreen title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                            <iframe src={url} allowFullScreen title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
                         </div>
                     )
                 })}
@@ -40,6 +40,40 @@ const Videos = () => {
 
         </div>
     )
+}
+
+
+export const getStaticProps = async () => {
+
+    const links = [
+        'https://www.youtube.com/watch?v=dmtK7RiIz1A',
+        'https://www.youtube.com/watch?v=a5elivsTUKg',
+        'https://www.youtube.com/watch?v=5svxuS3L9_s',
+        'https://www.youtube.com/watch?v=wdOra_ckb-s',
+        'https://www.youtube.com/watch?v=3Gz-VEdWnx0',
+        'https://www.youtube.com/watch?v=LIDABfQvkkc'
+    ]
+    const test = async (id) => {
+        const response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&fields=items(id%2Csnippet)&key=${process.env.YTAPI}`)
+
+        return response
+    }
+
+    const obj = {}
+
+    links.map(async (item, i) => {
+        const response = await test(item.split('=')[1])
+        console.log(response.data.items[0])
+        // obj[i] = response
+    })
+
+    // console.log(obj)
+
+    return {
+        'props': {
+            'test': obj,
+        },
+    }
 }
 
 export default Videos
