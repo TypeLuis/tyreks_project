9e589e5ee89b2c9e520ac1404b62cd3c31af36eb
typeluis <typeluis@gmail.com>
2022-10-26 15:31:44 -0400

    adding ex links

--- pages/Videos.js
+++ pages/Videos.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Videos = () => {
+    const links = [
+        'https://www.youtube.com/watch?v=dmtK7RiIz1A',
+        'https://www.youtube.com/watch?v=a5elivsTUKg',
+        'https://www.youtube.com/watch?v=5svxuS3L9_s',
+        'https://www.youtube.com/watch?v=wdOra_ckb-s',
+        'https://www.youtube.com/watch?v=3Gz-VEdWnx0',
+        'https://www.youtube.com/watch?v=LIDABfQvkkc'
+    ]
     return (
         <div>
+
 
         </div>
     )
