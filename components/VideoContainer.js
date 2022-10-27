c31fcb26cb37753444f780b12b06743d33147f58
typeluis <typeluis@gmail.com>
2022-10-27 19:06:07 -0400

    removed info container

--- components/VideoContainer.js
+++ components/VideoContainer.js
@@ -15,7 +15,7 @@ const VideoContainer = (props) => {
     }, [clicked])
 
     const handleMouseOver = () => {
-        const iframe = infoRef.current?.parentNode.parentNode
+        const iframe = infoRef.current?.parentNode
         iframe.style.border = '1px solid green'
         if (clicked) {
             setInfoHeight(infoHeight - 50)
@@ -27,7 +27,7 @@ const VideoContainer = (props) => {
     }
 
     const handleMouseOut = () => {
-        const iframe = infoRef.current?.parentNode.parentNode
+        const iframe = infoRef.current?.parentNode
         iframe.style.border = '1px solid #eaeaea'
         if (clicked) {
             setInfoHeight(infoHeight + 50)
@@ -42,25 +42,28 @@ const VideoContainer = (props) => {
     const url = `https://www.youtube.com/embed/${videoId}`
     const title = info.snippet.title
     const description = info.snippet.description
+
     return (
         <div className={`${classes['Iframe_Container']} ${classes[checkclicked]}`}>
-            <h3>{title}</h3>
-            <iframe src={url} allowFullScreen title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            <div className={`${classes['info_container']}`}>
-                <h4 onMouseOver={() => { handleMouseOver() }} onMouseOut={() => { handleMouseOut() }} onClick={() => { setClicked(!clicked); }}>
-                    {clicked ?
-                        <>Less Info ↑</>
-                        :
-                        <>More Info ↓</>
-                    }
-                </h4>
 
-                <div ref={infoRef} style={{ "height": infoHeight }} className={`${classes['more-info']}`}>
-                    <p>
-                        description: {description}
-                    </p>
-                </div>
+            <h3>{title}</h3>
+
+            <iframe src={url} allowFullScreen title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+
+            <div ref={infoRef} style={{ "height": infoHeight }} className={`${classes['more-info']}`}>
+                <p>
+                    description: {description}
+                </p>
             </div>
+
+            <h4 onMouseOver={() => { handleMouseOver() }} onMouseOut={() => { handleMouseOut() }} onClick={() => { setClicked(!clicked); }}>
+                {clicked ?
+                    <>Less Info ↑</>
+                    :
+                    <>More Info ↓</>
+                }
+            </h4>
+
         </div >
     )
 }
