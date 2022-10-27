38b4ed2cf5d048a779f736e3f3bf3089e48dfec5
typeluis <typeluis@gmail.com>
2022-10-27 17:04:44 -0400

    calculating height

--- components/VideoContainer.js
+++ components/VideoContainer.js
@@ -1,25 +1,47 @@
 import React from 'react'
 import classes from '../styles/Videos.module.scss'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 const VideoContainer = (props) => {
     const [clicked, setClicked] = useState(false)
     const checkclicked = clicked ? 'clicked' : 'not_clicked'
+
+    const [infoHeight, setInfoHeight] = useState(null);
+    const infoRef = useRef(null)
+
+    useEffect(() => {
+        setInfoHeight(0)
+    }, [])
+
+    const calcHeight = () => {
+
+        const height = clicked ? infoRef.current?.scrollHeight : 0
+        setInfoHeight(height)
+    }
     const info = props.info
     const videoId = info.id
     const url = `https://www.youtube.com/embed/${videoId}`
     const title = info.snippet.title
     const description = info.snippet.description
     return (
-        <div className={classes['Iframe_Container']}>
+        <div className={`${classes['Iframe_Container']} ${classes[checkclicked]}`}>
             <h3>{title}</h3>
             <iframe src={url} allowFullScreen title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            <div className={`${classes['more-info']} ${classes[checkclicked]}`}>
-                <p>
-                    description: {description}
-                </p>
+            <div className={`${classes['info_container']}`}>
+                <h4 onClick={() => { setClicked(!clicked); console.log(infoRef); calcHeight() }}>
+                    {clicked ?
+                        <>Less Info ↑</>
+                        :
+                        <>More Info ↓</>
+                    }
+                </h4>
+
+                <div ref={infoRef} style={{ "height": infoHeight }} className={`${classes['more-info']}`}>
+                    <p>
+                        description: {description}
+                    </p>
+                </div>
             </div>
-            <h4>More Info ↓</h4>
         </div >
     )
 }
