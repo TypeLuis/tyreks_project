e64ade883276009aa8a32af67633c775e23ba0b3
typeluis <typeluis@gmail.com>
2022-10-26 20:18:09 -0400

    handleing uploads

--- pages/Videos.js
+++ pages/Videos.js
@@ -3,32 +3,15 @@ import React from 'react'
 import classes from '../styles/Videos.module.scss'
 
 const Videos = (props) => {
-    const links = [
-        'https://www.youtube.com/watch?v=dmtK7RiIz1A',
-        'https://www.youtube.com/watch?v=a5elivsTUKg',
-        'https://www.youtube.com/watch?v=5svxuS3L9_s',
-        'https://www.youtube.com/watch?v=wdOra_ckb-s',
-        'https://www.youtube.com/watch?v=3Gz-VEdWnx0',
-        'https://www.youtube.com/watch?v=LIDABfQvkkc'
-    ]
 
-    const test = async (id) => {
-        const response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&fields=items(id%2Csnippet)&key=${process.env.YTAPI}`)
-
-        return response
-    }
     console.log(props.test)
     return (
         <div className={classes.main}>
 
             <div className={classes.grid}>
 
-                {props.test.map((item, i) => {
+                {props.test && props.test.map((item, i) => {
                     const url = `https://www.youtube.com/embed/${item.id}`
-                    // const url = item.replace("watch?v=", "embed/")
-                    // const id = item.split('=')[1]
-                    // console.log(id)
-                    // console.log(response)
 
                     return (
                         <div className={classes['Iframe_Container']}>
@@ -84,11 +67,25 @@ export const getStaticProps = async () => {
         return result
     }
 
-    const result = await YouTube_Video_Info()
+    // https://stackoverflow.com/questions/18953499/youtube-api-to-fetch-all-videos-on-a-channel
+    const test = async () => {
+        try {
+
+            const response = await axios.get(`https://www.googleapis.com/youtube/v3/channels?id=UC-Y-ZFvEtINXQADTc3_3C9Q&key=${process.env.YTAPI}&part=snippet,contentDetails,statistics`)
+
+            console.log(response.data.items[0].contentDetails.relatedPlaylists.uploads)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // console.log(test)
+    test()
+    // const result = await YouTube_Video_Info()
 
     return {
         'props': {
-            'test': result,
+            // 'test': result,
         },
     }
 }
