e7beeca8c6dc08ed7359ee6f2b0b804dcfead117
typeluis <typeluis@gmail.com>
2022-10-28 14:11:16 -0400

    page content

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import Header from '../components/Header'
 import '../styles/globals.css'
+import classes from '../styles/global.module.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <div className={classes.Page_Content}>
+
+        <div>
+
+        </div>
+        <Component {...pageProps} />
+        <div>
+
+        </div>
+
+      </div>
 
     </>
   )
