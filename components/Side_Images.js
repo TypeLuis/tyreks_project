c8aaa0b2c7ed83687f7aee1ebb41767a8bf39ef4
typeluis <typeluis@gmail.com>
2022-10-29 20:49:05 -0400

    changing side images

--- components/Side_Images.js
+++ components/Side_Images.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import classes from '../styles/SideImages.module.scss'
 
-import Beige from '../Assets/Death_Trap_Beige.jpeg'
+import Beige from '../Assets/Death_Trap_Beige.png'
 import Colored from '../Assets/Death_Trap_Colored.png'
-import Dark_Green from '../Assets/Death_Trap_Dark_Green.jpeg'
-import Purple from '../Assets/Death_Trap_Purple.jpeg'
-import Red from '../Assets/Death_Trap_Red.jpeg'
+import Dark_Green from '../Assets/Death_Trap_Dark_Green.png'
+import Purple from '../Assets/Death_Trap_Purple.png'
+import Red from '../Assets/Death_Trap_Red.png'
 // import Blue from '../Assets/Death_Trap_Blue.jpeg'
 
 const Side_Images = (props) => {
