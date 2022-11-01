d2b8f920ce4e14284f087f80fe239d960e9ab5ee
typeluis <typeluis@gmail.com>
2022-11-01 17:41:14 -0400

    add shop page

---
+++ pages/Shop/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from '../../styles/Shop.module.scss'
+
+const Shop = () => {
+    const style = {
+        'background-color': 'blue'
+    }
+    return (
+        <div style={style}>Shop</div>
+    )
+}
+
+export default Shop
