3f314d84b09c51f5f6436bda85d1633222f9aaaf
typeluis <typeluis@gmail.com>
2022-11-04 18:29:24 -0400

    fixing images

--- components/Card.js
+++ components/Card.js
@@ -193,7 +193,7 @@ const Card = (props) => {
             <div className={classes.product__image}>
                 {/* <img src='https://images.unsplash.com/photo-1648326311535-21895c185fbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80' /> */}
 
-                {props.slider ?
+                {/* {props.slider ?
 
                     // React.js
                     // <img src={images[0]}></img>
@@ -218,7 +218,7 @@ const Card = (props) => {
                         })}
                     </div>
 
-                }
+                } */}
             </div>
 
             {/* <div className={classes.product__wave}>
