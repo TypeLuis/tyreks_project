f5db288262810e619e8dd20dabef9f49d2ba5055
typeluis <typeluis@gmail.com>
2022-11-04 17:23:59 -0400

    comment

--- pages/Shop/index.js
+++ pages/Shop/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../../components/Card'
+// import Card from '../../components/Card'
 import classes from '../../styles/Shop.module.scss'
 
 const Shop = () => {
