d8460e3da9a339ac7df05f29b943151adb2398ac
typeluis <typeluis@gmail.com>
2022-11-05 12:44:10 -0400

    removed ref

--- pages/Donate.js
+++ pages/Donate.js
@@ -1,18 +1,11 @@
 import React, { useEffect } from 'react'
 import classes from '../styles/Donate.module.scss'
-import { useRef } from 'react'
 
 const Donate = () => {
-    const ref = useRef()
-
-    useEffect(() => {
-        console.log(ref)
-    }, [])
 
     const frameStyle = {
-
-        // "height": "max-content",
         "padding": '125px 50px 100px 25px',
+        "margin-left": '25px',
 
         "max-width": "500px",
         "min-width": "250px",
@@ -23,7 +16,7 @@ const Donate = () => {
             <h1>Donate</h1>
             <a className={classes['Donation_Box']} href="https://donorbox.org/zombie-killer-death-trap" target="_blank" rel="noopener noreferrer">Donate</a>
 
-            <iframe ref={ref} src="https://donorbox.org/embed/zombie-killer-death-trap" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height="700px" width="100%" style={frameStyle}></iframe>
+            <iframe src="https://donorbox.org/embed/zombie-killer-death-trap" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height="700px" width="100%" style={frameStyle}></iframe>
         </div>
     )
 }
