9e359487f7fbd270faaef261ec8e9b659585c7f7
typeluis <typeluis@gmail.com>
2022-11-05 20:38:19 -0400

    hidding game

--- pages/Shop/index.js
+++ pages/Shop/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from '../../components/Card'
-import Images from '../../components/images'
+import Images from '../../components/Images'
 import classes from '../../styles/Shop.module.scss'
 
 
