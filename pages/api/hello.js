2d69903f74e5ebca2efa6c9ca31ed0c74cd35be0
typeluis <typeluis@gmail.com>
2022-11-15 15:12:35 -0500

    starting api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+
+// More info on how to parse Query Strings
+// https://upmostly.com/nextjs/how-to-parse-query-string-parameters-in-next-js
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const { pid } = req.query
+  console.log(req.query)
+  res.status(200).json({ name: 'John Doe', query: pid ? pid : 'None', dick: 'chaney' })
 }
