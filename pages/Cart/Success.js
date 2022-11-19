4ade4999d5ed4bef0c2858f7986d09be71c022ab
typeluis <typeluis@gmail.com>
2022-11-19 15:34:31 -0500

    styling success

--- pages/Cart/Success.js
+++ pages/Cart/Success.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import classes from '../../styles/SuccessOrCancel.scss'
+import classes from '../../styles/SuccessOrCancel.module.scss'
 
 const Success = () => {
     const [customerData, setCustomerData] = useState()
@@ -21,11 +21,28 @@ const Success = () => {
         console.log(customerData)
     }
     return (
-        <div>
+        <div className={classes.Success}>
             {customerData &&
 
                 <section className={classes['order-info']} onClick={() => { handleTestClick() }}>
-                    {customerData.customer.email}
+                    {/* {customerData.customer.email} */}
+                    <h1>Thank you for your Order!</h1>
+                    <ul>
+                        <li>Name: <span>{customerData.customer.name}</span></li>
+                        <li>Email: <span>{customerData.customer.email}</span></li>
+                    </ul>
+
+                    <ul>
+                        <li>Order Total: <span>${customerData.session.amount_total / 100}</span></li>
+                        <li>Payment Status: <span>{customerData.session.payment_status}</span></li>
+                    </ul>
+
+                    <p>
+                        We appriciate your business!
+                        If you have any questions, please email
+                        <a href='mailto:orders@example.com'>orders@example.com</a>
+                    </p>
+
                 </section>
 
             }
