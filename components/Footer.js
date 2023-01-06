61b575ddc069e541c7f583dfb3b1d4bf2482fd0f
typeluis <typeluis@gmail.com>
2023-01-06 14:08:29 -0500

    styling

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,15 @@ import classes from '../styles/Footer.module.scss'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <div className='icons'>
+                <ul>
+                    <li id='github'><a title='Github' href='https://github.com/TypeLuis' target="_blank" rel="noopener noreferrer"></a></li>
+
+                    <li id='github'><a title='Github' href='https://github.com/TypeLuis' target="_blank" rel="noopener noreferrer"></a></li>
+                </ul>
+            </div>
+        </footer>
     )
 }
 
